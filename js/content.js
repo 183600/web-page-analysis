e9f5fc70fa761e183f2e71edc1a015f5e2869d0b
183600 <zhongwenjia2009@163.com>
2024-01-25 08:53:47 +0800

    改为网页分析

--- js/content.js
+++ js/content.js
@@ -2,4 +2,19 @@
 //本项目基于chrome-extensions-samples-main修改，并且此文件修改了
 var pageTitle = document.title;
 chrome.runtime.sendMessage({title: pageTitle});
+let allText = '';
+
+function getTextNodes(node) {
+  if (node.nodeType === Node.TEXT_NODE) {
+    allText += node.textContent.trim() + ' ';
+  } else {
+    for (let i = 0; i < node.childNodes.length; i++) {
+      getTextNodes(node.childNodes[i]);
+    }
+  }
+}
+
+getTextNodes(document.body);
+
+console.log(allText);
 
--- js/popup.js
+++ js/popup.js
@@ -1,10 +1,17 @@
 //Copyright 2024 183600
 //本项目基于chrome-extensions-samples-main修改，并且此文件修改了
 function getSource(){return "<html>"+document.getElementsByTagName('html')[0].innerHTML+"</html>";};
-    var str = getSource();
-console.log(str);
-alert(str);
-var zuizhongstr = new Array();
+//    var str = getSource();
+//console.log(getSource());
+//Copyright 2024 183600
+//本项目基于chrome-extensions-samples-main修改，并且此文件修改了
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var currentTab = tabs[0];
+    var pageTitle = currentTab.title;
+    // 然后你可以对获取到的标题进行操作，比如显示在popup页面上
+    console.log(pageTitle);
+    var str = pageTitle;
+    var zuizhongstr = new Array();
     const segmenterJa = new Intl.Segmenter("zh-CN", { granularity: "word" });
     const segments = segmenterJa.segment(str);
 marray = Array.from(segments)
@@ -12,29 +19,26 @@ marray = Array.from(segments)
 var buttonContainer = document.getElementById('buttonContainer');
 for (let index = 0; index < marray.length; index++) {
   var button = document.createElement('button');
-  button.innerHTML = marray[index].segment;
+  button.textContent = marray[index].segment;
       button.style.backgroundColor = 'lightblue';
   buttonContainer.appendChild(button);
+}
 
 // 添加点击事件
 var buttons = document.getElementsByTagName('button');
 for (var j = 0; j < buttons.length; j++) {
   buttons[j].addEventListener('click', function() {
-if (this.innerHTML === '复制') {
-console.log(this.innerHTML);
-    } else if (this.innerHTML === '搜索'){
-console.log(this.innerHTML);
+if (this.textContent === '复制') {
+console.log(this.textContent);
+    } else if (this.textContent === '搜索'){
+console.log(this.textContent);
     } else {
 if (this.style.backgroundColor === 'lightblue') {
       this.style.backgroundColor = 'royalblue';
       this.style.color = 'white';
-zuizhongstr[zuizhongstr.length] = this.innerHTML;
-    console.log(zuizhongstr.length,this.innerHTML);
+zuizhongstr[zuizhongstr.length] = this.textContent;
+    console.log(zuizhongstr.length,this.textContent);
     console.log(zuizhongstr);
-    } else {
-      this.style.backgroundColor = 'lightblue';
-      this.style.color = '#000000';
-zuizhongstr[zuizhongstr.length] = "";
     }
     }
   });
@@ -58,3 +62,4 @@ console.log(zuizhongstr2 , zuizhongstr);
 navigator.clipboard.writeText(zuizhongstr2);
 });
 });
+
