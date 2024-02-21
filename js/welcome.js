b637970f9ff24ef716cb11880501d94e17606f62
183600 <zhongwenjia2009@163.com>
2024-02-21 22:41:21 +0800

    添加关于获取网页词频，没做好

---
+++ js/welcome.js
@@ -0,0 +1,40 @@
+
+    // 找到要添加点击事件的按钮元素
+    var button = document.getElementById("gitee");
+
+    // 添加点击事件监听器
+    button.addEventListener("click", function() {
+        // 在点击事件发生后访问链接
+        window.location.href = "https://gitee.com/qwe12345678";
+        window.location.href = "https://gitee.com/qwe12345678";
+    });
+
+    // 找到要添加点击事件的按钮元素
+    var button = document.getElementById("github");
+
+    // 添加点击事件监听器
+    button.addEventListener("click", function() {
+        // 在点击事件发生后访问链接
+        window.location.href = "https://github.com/183600Qwe";
+        window.location.href = "https://github.com/183600Qwe";
+    });
+
+    // 找到要添加点击事件的按钮元素
+    var button = document.getElementById("gitcode");
+
+    // 添加点击事件监听器
+    button.addEventListener("click", function() {
+        // 在点击事件发生后访问链接
+        window.location.href = "https://gitcode.com/m0_49178139";
+        window.location.href = "https://gitcode.com/m0_49178139";
+    });
+
+    // 找到要添加点击事件的按钮元素
+    var button = document.getElementById("kuan");
+
+    // 添加点击事件监听器
+    button.addEventListener("click", function() {
+        // 在点击事件发生后访问链接
+        window.location.href = "https://www.coolapk.com/u/1910517";
+        window.location.href = "https://www.coolapk.com/u/1910517";
+    });
