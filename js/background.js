758866933b520ae219ad682696f1502e5a1d0e18
183600 <zhongwenjia2009@163.com>
2024-02-22 03:38:35 +0800

    添加关于获取网页词频，可能没做好

--- js/background.js
+++ js/background.js
@@ -1,7 +1,5 @@
 //Copyright 2024 183600
-//本项目基于chrome-extensions-samples-main修改，并且此文件修改了
-async function getCurrentTab() {
-  let queryOptions = { active: true, currentWindow: true };
-  let [tab] = await chrome.tabs.query(queryOptions);
-  return tab;
-}
+//本项目可能基于chrome-extensions-samples-main修改，并且此文件可能修改了
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.tabs.create({url: 'welcome.html'});
+});
--- js/welcome.js
+++ js/welcome.js
@@ -38,3 +38,13 @@
         window.location.href = "https://www.coolapk.com/u/1910517";
         window.location.href = "https://www.coolapk.com/u/1910517";
     });
+
+    // 找到要添加点击事件的按钮元素
+    var button = document.getElementById("yuanmagitee");
+
+    // 添加点击事件监听器
+    button.addEventListener("click", function() {
+        // 在点击事件发生后访问链接
+        window.location.href = "https://gitee.com/qwe12345678/web-page-analysis";
+        window.location.href = "https://gitee.com/qwe12345678/web-page-analysis";
+    });
