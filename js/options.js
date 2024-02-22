98268784b1480c7e20563dbd24e9c686cabb45ba
183600 <zhongwenjia2009@163.com>
2024-02-22 23:30:13 +0800

    添加关于获取网页词频

---
+++ js/options.js
@@ -0,0 +1,11 @@
+
+
+    // 找到要添加点击事件的按钮元素
+    var button = document.getElementById("guanyu");
+
+    // 添加点击事件监听器
+    button.addEventListener("click", function() {
+        // 在点击事件发生后访问链接
+        window.location.href = "welcome.html";
+        window.location.href = "welcome.html";
+    });
--- js/welcome.js
+++ js/welcome.js
@@ -48,3 +48,24 @@
         window.location.href = "https://gitee.com/qwe12345678/web-page-analysis";
         window.location.href = "https://gitee.com/qwe12345678/web-page-analysis";
     });
+
+
+    // 找到要添加点击事件的按钮元素
+    var button = document.getElementById("yuanmagitcode");
+
+    // 添加点击事件监听器
+    button.addEventListener("click", function() {
+        // 在点击事件发生后访问链接
+        window.location.href = "https://gitcode.com/m0_49178139/web-page-analysis/overview";
+        window.location.href = "https://gitcode.com/m0_49178139/web-page-analysis/overview";
+    });
+
+    // 找到要添加点击事件的按钮元素
+    var button = document.getElementById("qun");
+
+    // 添加点击事件监听器
+    button.addEventListener("click", function() {
+        // 在点击事件发生后访问链接
+        window.location.href = "http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=UbtLs1ReDi94nOH-CVfcXwMuNblBeiYD&authKey=%2BuJcSuT9YZ3z%2Faf7mCxE1W5KBiC7xDJUDZ1JR8qqqQ2Tlv7GSCoo98qX8I6%2B4g11&noverify=0&group_code=854260276";
+        window.location.href = "http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=UbtLs1ReDi94nOH-CVfcXwMuNblBeiYD&authKey=%2BuJcSuT9YZ3z%2Faf7mCxE1W5KBiC7xDJUDZ1JR8qqqQ2Tlv7GSCoo98qX8I6%2B4g11&noverify=0&group_code=854260276";
+    });
